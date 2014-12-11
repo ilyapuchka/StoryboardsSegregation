1e71c932f435e76eaccf010e4635419bc1a75dc3
Ilya Puchka <ilya@puchka.me>
2014-12-11 23:56:41 +0300

    simplified swizzling

--- Storyboards/Categories/UIViewController+Storyboards.m
+++ Storyboards/Categories/UIViewController+Storyboards.m
@@ -9,6 +9,8 @@
 #import "UIViewController+Storyboards.h"
 #import <objc/runtime.h>
 
+typedef void (*ObjCMsgSendReturnNil)(id, SEL);
+
 @implementation UIViewController (Storyboards)
 
 - (NSString *)storyboardName
@@ -40,6 +42,46 @@
         }
     }
     return nil;
+}
+
++ (void)load
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [self swizzleAwakeFromNib];
+    });
+}
+
++ (void)swizzleAwakeFromNib
+{
+    SEL sel = @selector(awakeFromNib);
+    Method method = class_getInstanceMethod([UIViewController class], sel);
+    ObjCMsgSendReturnNil originalImp = (ObjCMsgSendReturnNil)method_getImplementation(method);
+    
+    //UITabBarController and UINavigationController does not override -awakeFromNib, so we can swizzle UIViewController base implementation and check instance class.
+    IMP adjustedImp = imp_implementationWithBlock(^void(UINavigationController *instance) {
+        originalImp(instance, sel);
+        if ([instance isKindOfClass:[UINavigationController class]] ||
+            [instance isKindOfClass:[UITabBarController class]]) {
+            NSArray *newViewControllers = [instance viewControllersWithViewController:[instance viewControllers]];
+            [instance setViewControllers:newViewControllers];
+        }
+    });
+    
+    method_setImplementation(method, adjustedImp);
+}
+
+- (NSArray *)viewControllersWithViewController:(NSArray *)viewControllers
+{
+    NSMutableArray *mViewControllers = [viewControllers mutableCopy];
+    [viewControllers enumerateObjectsUsingBlock:^(UIViewController *vc, NSUInteger idx, BOOL *stop) {
+        UIViewController *newVC = [UIViewController viewControllerFromStoryboardWithName:vc.storyboardName
+                                                                withStoryboardIdentifier:vc.storyboardIdentifier];
+        if (newVC) {
+            [mViewControllers replaceObjectAtIndex:idx withObject:newVC];
+        }
+    }];
+    return [mViewControllers copy];
 }
 
 @end
